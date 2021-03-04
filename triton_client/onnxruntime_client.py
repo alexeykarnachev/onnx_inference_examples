199a792d19f157a6caad1cb7ce476ccfdd147a9a
Alexey Karnachev <alekseykarnachev@gmail.com>
2021-03-04 18:55:39 +0000

    Dirty onnx with caching implementation.

--- triton_client/onnxruntime_client.py
+++ triton_client/onnxruntime_client.py
@@ -3,47 +3,16 @@ import datetime
 
 import numpy as np
 import onnxruntime
-from transformers import GPT2TokenizerFast
+from onnxruntime.transformers.gpt2_helper import Gpt2Helper, Gpt2Inputs
+import torch
+from transformers import GPT2Config, GPT2TokenizerFast
 
-_BS = 8
-_PAST_SEQ_LEN = 0
-_N_REPEATS = 500
-_USE_FP16 = True
-_FLOAT_TYPE = np.float16 if _USE_FP16 else np.float32
-
-
-def main():
-    session = onnxruntime.InferenceSession("/workspace/distilgpt2.onnx")
-    inputs = session.get_inputs()
-    outputs = session.get_outputs()
-
-    input_names = [inp.name for inp in inputs]
-    output_names = [output.name for output in outputs]
-    input_ids = np.random.randint(low=0, high=100, size=(_BS, 1)).astype(np.int64)
-    position_ids = np.zeros_like(input_ids)
-    attention_mask = np.ones(shape=(_BS, 1), dtype=_FLOAT_TYPE)
-    inputs = {'input_ids': input_ids, 'position_ids': position_ids, 'attention_mask': attention_mask}
-    pasts = {
-        name: np.random.randn(*[2, _BS, 12, _PAST_SEQ_LEN, 64]).astype(_FLOAT_TYPE)
-        for name in input_names
-        if 'past' in name
-    }
-    inputs.update(pasts)
-
-    start = datetime.datetime.now()
-    for i in range(_N_REPEATS):
-        _ = session.run(output_names, inputs)
-        print(i)
-    end = datetime.datetime.now()
-    delta = end - start
-    time_per_call = delta / _N_REPEATS
-    time_per_call_ms = time_per_call.microseconds / 1000
-    print(time_per_call_ms)
+from logits_modifiers import IgnoredTokensModifier, TemperatureModifier, TopKNucleusModifier
 
 
 class GPT2Onnx:
-    def __init__(self, onnx_file_path, tokenizer_name_or_path, dialog_turn_token, max_n_tokens, max_n_context_tokens,
-                 use_fp16, cuda_id):
+    def __init__(self, onnx_file_path, model_name_or_path, dialog_turn_token, max_n_tokens, max_n_context_tokens,
+                 is_float16, cuda_id):
         assert max_n_tokens > max_n_context_tokens, 'max_n_tokens must be > max_n_context_tokens'
 
         self._session = onnxruntime.InferenceSession(onnx_file_path)
@@ -52,51 +21,134 @@ class GPT2Onnx:
         self._dialog_turn_token = dialog_turn_token
         self._max_n_tokens = max_n_tokens
         self._max_n_context_tokens = max_n_context_tokens
-        self._tokenizer = GPT2TokenizerFast.from_pretrained(tokenizer_name_or_path)
-        self._float_type = np.float16 if use_fp16 else np.float32
+        self._tokenizer = GPT2TokenizerFast.from_pretrained(model_name_or_path)
+        self._gpt2_config = GPT2Config.from_pretrained(model_name_or_path)
+        self._n_layers = self._gpt2_config.num_hidden_layers
         self._cuda_id = cuda_id
-
+        self._device = f'cuda:{cuda_id}'
+        self._is_float16 = is_float16
+        self._torch_float_type = torch.float16 if self._is_float16 else torch.float32
         self._io_binding = self._session.io_binding()
 
     def generate_candidates(self, context, n_candidates):
-        assert isinstance(context, collections.abc.Sequence) and not isinstance(context, str)
-        prefix = self._dialog_turn_token.join(context)
+        prefix = self._prepare_prefix(context)
         encoded_prefix = self._tokenizer.encode(prefix, add_special_tokens=False)
         encoded_prefix = encoded_prefix[-self._max_n_context_tokens:]
+        prefix_seq_len = len(encoded_prefix)
         encoded_prefixes = [encoded_prefix[:] for _ in range(n_candidates)]
 
-        input_ids = np.array(encoded_prefixes, dtype=np.int64)
-        position_ids = np.tile(np.arange(len(encoded_prefix)), (n_candidates, 1))
+        max_output_shapes = Gpt2Helper.get_output_shapes(
+            batch_size=n_candidates,
+            past_sequence_length=self._max_n_tokens,
+            sequence_length=self._max_n_tokens,
+            config=self._gpt2_config)
+
+        input_ids = torch.tensor(encoded_prefixes, dtype=torch.long, device=self._device)
+        position_ids = torch.arange(prefix_seq_len, dtype=torch.long, device=self._device).repeat((n_candidates, 1))
         past = self._get_empty_past(n_candidates)
-        attention_mask = np.ones((n_candidates, input_ids.shape[1]), dtype=self._float_type)
+        attention_mask = torch.ones((n_candidates, prefix_seq_len), dtype=self._torch_float_type, device=self._device)
+        gpt2_inputs = Gpt2Inputs(input_ids, position_ids, attention_mask, past)
 
-        inputs = {'input_ids': input_ids, 'position_ids': position_ids, 'attention_mask': attention_mask}
-        inputs.update(past)
+        output_buffers = Gpt2Helper.get_output_buffers(max_output_shapes, self._device, is_float16=self._is_float16)
+        output_shapes = Gpt2Helper.get_output_shapes(
+            batch_size=n_candidates, past_sequence_length=0, sequence_length=prefix_seq_len, config=self._gpt2_config)
+        ort_outputs = Gpt2Helper.onnxruntime_inference_with_binded_io(
+            ort_session=self._session,
+            inputs=gpt2_inputs,
+            output_buffers=output_buffers,
+            output_shapes=output_shapes,
+            return_numpy=False)
 
-        for input_name, input_numpy_arr in inputs.items():
-            input_ortvalue = onnxruntime.OrtValue.ortvalue_from_numpy(input_numpy_arr, 'cuda', 0)
-            self._io_binding.bind_ortvalue_input(input_name, input_ortvalue)
+        # ================================================================================
+        # Continue inference after the first step (using past):
+        generated_input_ids = []
 
-        for output_name in self._output_names:
-            self._io_binding.bind_output(output_name, 'cuda', 0)
+        for i_step in range(1):
+            next_token_logits = ort_outputs[0][:, -1, :]
 
-        self._session.run_with_iobinding(self._io_binding)
-        output_ortvalue = self._io_binding.get_outputs()[0]
+            _modify_next_token_logits(next_token_logits, ignored_input_ids=None, temperature=0.7, top_k=50, top_p=1.0)
 
-        print(output_ortvalue.device_name())
+            next_input_ids = _sample_next_input_ids(next_token_logits)
+            generated_input_ids.append(next_input_ids)
+
+#            past = ort_outputs[1:]
+#
+#            input_ids = next_input_ids[:, None]
+#            past_sequence_length = past[0].size(3)
+#            new_seq_len = 1 + past_sequence_length
+#
+#            position_ids = torch.tensor(
+#                [new_seq_len - 1], dtype=torch.long, device=self._device).repeat((n_candidates, 1))
+#            attention_mask = torch.ones((n_candidates, new_seq_len), dtype=self._torch_float_type, device=self._device)
+#
+#            gpt2_inputs = Gpt2Inputs(input_ids, position_ids, attention_mask, past)
+#            output_shapes = Gpt2Helper.get_output_shapes(
+#                batch_size=n_candidates,
+#                past_sequence_length=past_sequence_length,
+#                sequence_length=1,
+#                config=self._gpt2_config)
+#
+#            ort_outputs = Gpt2Helper.onnxruntime_inference_with_binded_io(
+#                ort_session=self._session,
+#                inputs=gpt2_inputs,
+#                output_buffers=output_buffers,
+#                output_shapes=output_shapes,
+#                return_numpy=False)
+#
+#        generated_input_ids = torch.stack(generated_input_ids)
+#
+#        generated_input_ids = generated_input_ids.cpu().numpy().T.tolist()
+#        decoded_candidates = self._tokenizer.batch_decode(generated_input_ids)
+#
+#        return decoded_candidates
+
+    def _prepare_prefix(self, context):
+        assert isinstance(context, collections.abc.Sequence) and not isinstance(context, str)
+        prefix = self._dialog_turn_token.join(context) + self._dialog_turn_token
+
+        return prefix
 
     def _get_empty_past(self, n_candidates):
-        past = {
-            name: np.random.randn(*[2, n_candidates, 12, 0, 64]).astype(self._float_type)
-            for name in self._input_names
-            if 'past' in name
-        }
+        past = [
+            torch.randn(
+                *[
+                    2, n_candidates, self._gpt2_config.num_attention_heads, 0,
+                    int(self._gpt2_config.n_embd / self._gpt2_config.n_head)
+                ],
+                device=self._device,
+                dtype=self._torch_float_type) for _ in range(self._n_layers)
+        ]
 
         return past
 
 
+def _sample_next_input_ids(next_token_logits):
+    probabilities = torch.nn.functional.softmax(next_token_logits, dim=-1)
+    next_tokens = torch.multinomial(probabilities, num_samples=1)
+    return next_tokens.squeeze(1)
+
+
+def _modify_next_token_logits(next_token_logits, ignored_input_ids, temperature, top_k, top_p):
+    modifiers = [
+        IgnoredTokensModifier(ignored_input_ids=ignored_input_ids),
+        TemperatureModifier(temperature=temperature),
+        TopKNucleusModifier(top_k=top_k, top_p=top_p)
+    ]
+
+    _ = [modifier(next_token_logits) for modifier in modifiers]
+
+
 if __name__ == '__main__':
-    model = GPT2Onnx("/workspace/distilgpt2.onnx", 'gpt2', '->', 100, 50, True, 0)
-    context = ["What is your name?", "My name is Alex", "How old are you?"]
+    model = GPT2Onnx('/workspace/triton_client/gpt2-large/gpt2-large_past_fp16/gpt2-large_past_fp16.onnx', 'gpt2-large',
+            '->', 51, 50, True, 0)
+    context = ["What is your name?", "My name is Alex", "How old are you?"] * 100
+    n_repeats = 1000
     n_candidates = 8
-    model.generate_candidates(context, n_candidates)
+    start_time = datetime.datetime.now()
+    for i in range(n_repeats):
+        model.generate_candidates(context, n_candidates)
+        print(i)
+
+    end_time = datetime.datetime.now()
+    delta = (end_time - start_time) / n_repeats
+    print(delta)
